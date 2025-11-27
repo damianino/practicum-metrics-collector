b4a1e60148210faadd3f1dbac9a622fd8b7b5ffd
Alessandro Lo Cicero Vaina <cicero956@gmail.com>
2025-11-27 20:13:11 +0300

    attempt 1

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,22 @@
 package main
 
-func main() {}
+import (
+	"github.com/damianino/practicum-metrics-collector/internal/handler"
+	"github.com/damianino/practicum-metrics-collector/internal/service/memstorage"
+	"log"
+	"net/http"
+)
+
+func main() {
+	ms := memstorage.NewMemStorage()
+
+	h := handler.NewHandler(ms)
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/update/gauge/{metricName}/{metricValue}", h.SetGauge)
+	mux.HandleFunc("/update/counter/{metricName}/{metricValue}", h.IncCounter)
+
+	if err := http.ListenAndServe(":8080", mux); err != nil {
+		log.Fatal(err)
+	}
+}
